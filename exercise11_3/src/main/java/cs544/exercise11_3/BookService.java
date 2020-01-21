a527b1002a7545e5869f8f0a186cb7e9960b0356
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-01-21 14:39:06 -0600

    Added exercise11_3

---
+++ exercise11_3/src/main/java/cs544/exercise11_3/BookService.java
@@ -0,0 +1,36 @@
+package cs544.exercise11_3;
+
+import java.util.*;
+
+public class BookService implements IBookService {
+	public List<IBookSupplier> suppliers;
+
+	public BookService() { }
+
+	public void setSuppliers(List<IBookSupplier> iBookSupplier) {
+		this.suppliers = iBookSupplier;
+	}
+
+	public void buy(Book book) {
+		double lowestPrice = 0;
+		IBookSupplier cheapestSupplier = null;
+		// find the cheapest supplier
+		for (IBookSupplier supplier : suppliers) {
+			double price = supplier.computePrice(book.getIsbn());
+			if (cheapestSupplier == null) {
+				cheapestSupplier = supplier;
+				lowestPrice = price;
+			} else {
+				if (price < lowestPrice) {
+					cheapestSupplier = supplier;
+					lowestPrice = price;
+				}
+			}
+		}
+		// buy with the cheapest supplier
+		if (cheapestSupplier != null) {
+			cheapestSupplier.order(book);
+		}
+
+	}
+}
