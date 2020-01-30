8f77b282ead37870db6e3421d52100f07d23f496
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-01-30 16:25:43 -0600

    Moved microservice url from code to properties

--- spring-cloud-services-labs/css544-lab01-greeting/src/main/java/edu/miu/cs/cs544/labs/lab01/service/FortuneServiceClient.java
+++ spring-cloud-services-labs/css544-lab01-greeting/src/main/java/edu/miu/cs/cs544/labs/lab01/service/FortuneServiceClient.java
@@ -1,5 +1,6 @@
 package edu.miu.cs.cs544.labs.lab01.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,12 +9,15 @@ import edu.miu.cs.cs544.labs.lab01.domain.Fortune;
 @Service
 public class FortuneServiceClient implements FortuneService {
 	private RestTemplate restTemplate;
+
+	@Value("${services.fortune.baseUrl}")
+	private String fortuneServiceUrl;
 	
 	public FortuneServiceClient(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public Fortune getFortune() {
-		return restTemplate.getForObject("http://FORTUNE/", Fortune.class);
+		return restTemplate.getForObject(fortuneServiceUrl, Fortune.class);
 	}
 }
